1b1feaf3ec36e88cc0b6e1ecde9cbc757ea97c1e
SauloHA26 <saulo-gamaliel@hotmail.com>
2026-05-18 20:56:48 -0600

    Deploy: Tracker Financiero 2026 PWA v1.0

---
+++ sw.js
@@ -0,0 +1,39 @@
+const CACHE = 'tracker-v1'
+
+const PRECACHE = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+]
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(PRECACHE)).then(() => self.skipWaiting())
+  )
+})
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  )
+})
+
+self.addEventListener('fetch', e => {
+  // Only cache same-origin GET requests
+  if (e.request.method !== 'GET' || !e.request.url.startsWith(self.location.origin)) return
+
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      const network = fetch(e.request).then(response => {
+        if (response.ok) {
+          const clone = response.clone()
+          caches.open(CACHE).then(c => c.put(e.request, clone))
+        }
+        return response
+      }).catch(() => cached)
+      return cached || network
+    })
+  )
+})
